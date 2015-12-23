888315721d93c6e016b34c2bc72e5a80b29e187c
MSongJiHyo <2589481930@qq.com>
2015-12-23 19:14:17 +0800

    Create TreeNode.h

---
+++ TreeNode.h
@@ -0,0 +1,26 @@
+#ifndef TreeNode_H
+#define	TreeNode_H
+enum TreeNodeKind{
+	CHAR,
+	CONCAT,
+	ALT,
+	CLOSURE
+};
+class TreeNode
+{
+public:
+	TreeNodeKind nodeKind;
+	char c;
+	TreeNode *left;
+	TreeNode *right;
+
+	TreeNode(TreeNodeKind nodeKind_, char c_, TreeNode *left_, TreeNode* right_)
+	{
+		nodeKind = nodeKind_;
+		c = c_;
+		left = left_;
+		right = right_;
+	}
+};
+
+#endif
