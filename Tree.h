bc54d16c0f2758246528bc0b6ccf7c08f71cb848
MSongJiHyo <2589481930@qq.com>
2015-12-24 16:36:00 +0800

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -5,16 +5,18 @@ class Tree
 {
 public:
 	char currentChar;
+	char peerChar;
 	string exp;
 	string::size_type pos;
 	TreeNode *root;
 	char	  getNextChar();
+	char      getBackChar();
 	TreeNode *ReToTree();
 	TreeNode *parse_Exp();
 	TreeNode *parse_B();
 	TreeNode *parse_A();
 	TreeNode *parse_C();
-
+	void Re_print(TreeNode* root);
 	Tree(string);
 	
 };
