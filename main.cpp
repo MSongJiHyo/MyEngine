cfbf5947ce6dbc2185eaae267404717548229526
MSongJiHyo <2589481930@qq.com>
2015-12-24 12:26:02 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Tree.h"
+#include "TreeNode.h"
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+	Tree::Tree ("(ab)*|c");
+	system("pause");
+	return 0;
+}
